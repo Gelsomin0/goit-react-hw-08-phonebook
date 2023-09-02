216567c01bc2aabab2b054a770e54e70b773edbb
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-09-02 16:51:06 +0300

    Some changes

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,19 @@
 import { AccountCircle, Mail, Password } from "@mui/icons-material";
 import { Box, Button, Paper, TextField } from "@mui/material";
+import { useState } from "react";
 
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    const handleFormData = ({target}) => {
+        if (target.name === 'name') setName(target.value);
+        if (target.name === 'email') setEmail(target.value);
+        if (target.name === 'password') setPassword(target.value);
+    }
+
     return (
         <div>
             <form>
@@ -12,20 +24,40 @@ const Register = () => {
                         flexDirection: 'column',
                         gap: 2,
                         alignItems: 'center',
-                        padding: '25px 30px'
+                        padding: '25px 30px',
+                        width: '400px',
+                        margin: '0 auto'
                     }}
                 >
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <AccountCircle  sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField label='Name' variant='standard' />
+                        <TextField
+                            name='name'
+                            value={name}
+                            label='Name'
+                            variant='standard'
+                            onChange={handleFormData}
+                        />
                     </Box>
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <Mail  sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField label='E-mail' variant='standard' />
+                        <TextField
+                            name='email'
+                            value={email}
+                            label='E-mail'
+                            variant='standard'
+                            onChange={handleFormData}
+                        />
                     </Box>
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <Password  sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                        <TextField label='Password' variant='standard' />
+                        <TextField
+                            name='password'
+                            value={password}
+                            label='Password'
+                            variant='standard'
+                            onChange={handleFormData}
+                        />
                     </Box>
                     <Button variant='contained'>Create account</Button>
                 </Paper>
